eca8cab754e5e32e377b1dd909a17aca9584f492
Chathuratharushka <chathuratharushka@gmail.com>
2022-08-22 13:47:31 +0530

    made some changes in UI.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,14 +12,14 @@ import { render } from "@testing-library/react";
 
 
 
-function Home(){
-    
-    
-    return(
+function Home() {
+
+
+    return (
         <div className="homeBody">
-            <div className='header'><Header/></div>
+            <div className='header'><Header /></div>
             <div className="homeMainContainer">
-                 <div className="homeImageContainer">
+                <div className="homeImageContainer">
                     <img className="homeImage" src={homeImage} alt="Logo" />
                 </div>
                 <div className="loginContainer">
@@ -28,18 +28,18 @@ function Home(){
                         <img className="logoImage" src={logo} alt="Logo" />
                     </div>
                     <div className="buttons">
-                        
-                        
+
+
                         <h6 className="textOne">Don't have an account?</h6>
                         <a href={"https://accounts.asgardeo.io/t/awsgramacheck/accountrecoveryendpoint/register.do?client_id=rgfykPFL3gb_U3sJL1jIzKJbfRMa&code_challenge=AvtUGsul-IfhmZAowA8ohxgZA6Vv4j7aXIVF9HwkEcY&code_challenge_method=S256&commonAuthCallerPath=%2Ft%2Fawsgramacheck%2Foauth2%2Fauthorize&forceAuth=false&passiveAuth=false&redirect_uri=https%3A%2F%2Fd1lidqfrgb5glk.cloudfront.net%2Fmain&response_mode=query&response_type=code&scope=openid+profile&state=request_0&sessionDataKey=a18b57bc-f725-4955-b02d-c23b54c7bf94&relyi"}><h6 className="signupText" >Sign Up </h6></a>
                     </div>
                 </div>
-                
-                
+
+
 
             </div>
             <div className='footer'>
-                <Footer/>
+                <Footer />
             </div>
         </div>
     )
