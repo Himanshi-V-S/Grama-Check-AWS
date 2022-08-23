10aebf904d12badf4c5a033d4930ff50ca48cedd
Chathuratharushka <chathuratharushka@gmail.com>
2022-08-23 09:12:59 +0530

    signup button and help button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,7 @@ function Home() {
 
 
                         <h6 className="textOne">Don't have an account?</h6>
-                        <a href={"https://accounts.asgardeo.io/t/awsgramacheck/accountrecoveryendpoint/register.do?client_id=rgfykPFL3gb_U3sJL1jIzKJbfRMa&code_challenge=AvtUGsul-IfhmZAowA8ohxgZA6Vv4j7aXIVF9HwkEcY&code_challenge_method=S256&commonAuthCallerPath=%2Ft%2Fawsgramacheck%2Foauth2%2Fauthorize&forceAuth=false&passiveAuth=false&redirect_uri=https%3A%2F%2Fd1lidqfrgb5glk.cloudfront.net%2Fmain&response_mode=query&response_type=code&scope=openid+profile&state=request_0&sessionDataKey=a18b57bc-f725-4955-b02d-c23b54c7bf94&relyi"}><h6 className="signupText" >Sign Up </h6></a>
+                        <a className="signupLink" href={"https://accounts.asgardeo.io/t/awsgramacheck/accountrecoveryendpoint/register.do?client_id=rgfykPFL3gb_U3sJL1jIzKJbfRMa&code_challenge=AvtUGsul-IfhmZAowA8ohxgZA6Vv4j7aXIVF9HwkEcY&code_challenge_method=S256&commonAuthCallerPath=%2Ft%2Fawsgramacheck%2Foauth2%2Fauthorize&forceAuth=false&passiveAuth=false&redirect_uri=https%3A%2F%2Fd1lidqfrgb5glk.cloudfront.net%2Fmain&response_mode=query&response_type=code&scope=openid+profile&state=request_0&sessionDataKey=a18b57bc-f725-4955-b02d-c23b54c7bf94&relyi"}><h6 className="signupText" >Sign Up </h6></a>
                     </div>
                 </div>
 
